ab004fa3123d97b442763ec339327397706c62b5
chinulz <86215698+chinulz@users.noreply.github.com>
2022-03-09 22:48:26 +0700

    Commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+function sukses() {
+	swal.fire('Halo Selamat Datang');
+}
+
+function login() {
+	alert('Login Dulu');
+	var usr = prompt("Masukan Username");
+	var pass = prompt("Masukan Password");
+	
+	if (usr != 'admin') {
+		swal.fire('Gagal', 'Username atau Password Salah!', 'error');
+	} else if (pass != 'admin') {
+		swal.fire('Gagal', 'Username atau Password Salah!', 'error');
+	} else {
+		swal.fire('Berhasil', 'Kamu Telah Login', 'success');
+		var i = 0, text;
+		text = "Selamat Anda Berhasil Login...";
+		
+		function typing() {
+		if (i < text.length) {
+		document.getElementById('text').innerHTML += text.charAt(i);
+		i++;
+		setTimeout(typing, 350);
+    	}
+      } typing();
+      document.getElementById('tombol').innerHTML = `Berhasil`;
+      window.setTimeout("window.close();", 13000)
+	} 
+ }
